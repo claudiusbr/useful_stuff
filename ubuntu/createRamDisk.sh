aba10d15e22966e37bcda6a0f07257a8c3733ad6
claudiusbr <claudiusbr@users.noreply.github.com>
2018-01-22 17:44:50 +0000

    now without sudo!

--- ubuntu/createRamDisk.sh
+++ ubuntu/createRamDisk.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# This script mounts tmpfs in a directory in your home folder.
-# I use this to work on files which are encrypted on disk, in
-# order to make sure that their plain-text versions do not
-# persist. If your home folder is already encrypted, even
-# better -- this adds a 'second layer' of 'protection'.
-# since this uses `mount` with options, you must be a
-# sudoer to execute it
-if [ ! -e "$HOME/etc/tmp" ]; then
-  mkdir -p "$HOME/etc/tmp"
-fi
+# create a link to /dev/shm/{username}tmp in your home directory. I use this to
+# work on files which are encrypted on disk, in order to make sure that their
+# plain-text versions do not persist.  If your home folder is already
+# encrypted, even better -- this adds a 'second layer' of 'protection'.  since
+# this uses `mount` with options, you must be a sudoer to execute it
+if [ -e "/dev/shm" ]; then
+  if [ ! -e "/dev/shm/$USER"'tmp' ]; then
+    mkdir "/dev/shm/$USER"'tmp'
+  fi
 
-mount -t tmpfs -o size=256m tmpfs '$HOME/etc/tmp'
+  if [ ! -e "$HOME/etc" ]; then
+    mkdir "$HOME/etc"
+  elif [ -d "$HOME/etc/tmp" ]; then
+    rm -rf "$HOME/etc/tmp"
+  fi
+
+  ln -sTf "/dev/shm/$USER"'tmp' "$HOME/etc/tmp"
+else
+  echo "This script needs the '/dev/shm' device to work, but it was not found on this machine" 1>&2
+fi
