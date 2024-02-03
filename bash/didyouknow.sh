fd2d60b67d9e4fdd31ed8115fa4465180154cabe
Claudius <13047598+claudiusbr@users.noreply.github.com>
2024-02-03 22:50:49 +0000

    make the script always run in bash

--- bash/didyouknow.sh
+++ bash/didyouknow.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 # this command will display a random fact from the Wikipedia Did You Know? page
 # TODO: add a check to see if jq, curl and sed are installed
 # TODO: add logic in case it doesn't return anything
-echo -e '\nDid You Know? ...' && \
+printf '\nDid You Know? ...\n' && \
   curl https://en.wikipedia.org/w/api.php\?action\=query\&prop\=extracts\&titles\=Template%3aDid_you_know\&format\=json\&explaintext 2> stats.txt | \
   jq '.query.pages | to_entries[] | .value.extract' | \
   sed 's/"\\n== Next update ==\\n\\n\\n== Hooks ==\\n\\n//g' | \
