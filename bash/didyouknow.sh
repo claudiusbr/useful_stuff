0bc6a04444aef2abb97fdb9f88c8eafe454719f0
Claudius <13047598+claudiusbr@users.noreply.github.com>
2024-02-03 23:05:56 +0000

    change curl to silent mode

--- bash/didyouknow.sh
+++ bash/didyouknow.sh
@@ -3,7 +3,7 @@
 # TODO: add a check to see if jq, curl and sed are installed
 # TODO: add logic in case it doesn't return anything
 printf '\nDid You Know? ...\n' && \
-  curl https://en.wikipedia.org/w/api.php\?action\=query\&prop\=extracts\&titles\=Template%3aDid_you_know\&format\=json\&explaintext 2> stats.txt | \
+  curl -s https://en.wikipedia.org/w/api.php\?action\=query\&prop\=extracts\&titles\=Template%3aDid_you_know\&format\=json\&explaintext | \
   jq '.query.pages | to_entries[] | .value.extract' | \
   sed 's/"\\n== Next update ==\\n\\n\\n== Hooks ==\\n\\n//g' | \
   sed 's/\\n/\
